81096ad96393579c91f04991a8b24f19d2e7a393
avinash13777 <avinash13777@gmail.com>
2026-03-15 18:37:14 +0530

    adding the square root feature 1

--- calculator.py
+++ calculator.py
@@ -1,32 +1,26 @@
 def add(self, a, b):
-
-return a + b
+   return a + b
 
 def subtract(self, a, b):
-
-return a - b
+   return a - b
 
 def multiply(self, a, b):
-
-return a * b
+   return a * b
 
 
 
 def divide(self, a, b):
-
-return a / b
+   return a / b
 
 # TODO: Implement the following function to calculate the square root of a number.
 
-# def square_root(self, x):
-
-# return math.sqrt(x)
+ def square_root(self, x):
+  return math.sqrt(x)
 
 # You need to uncomment the above function and complete its implementation to add the square root feature.
 
 if __name__ == "__main__":
-
-calculator = Calculator()
+   calculator = Calculator()
 
 num1 = 16
 
@@ -40,6 +34,5 @@ print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
 
 # TODO: Uncomment and test the square root feature.
 
-# num3 = 25
-
-# print(f"The square root of {num3} = {calculator.square_root(num3)}")
+ num3 = 25
+ print(f"The square root of {num3} = {calculator.square_root(num3)}")
